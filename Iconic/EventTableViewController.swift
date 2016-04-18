c37c123c16317f2ddf96576eb7cac789116a6c74
aburnight <alexis-burnight@uiowa.edu>
2016-04-18 15:31:40 -0500

    things are happening hehe

--- Iconic/EventTableViewController.swift
+++ Iconic/EventTableViewController.swift
@@ -14,17 +14,17 @@ class EventTableViewController: UITableViewController {
     // MARK: Event List
     var events = [SingleEvent]() //this will be pulled from the database
     var sendEvent : SingleEvent?
+    var eventTempImage: UIImage!
     var favorites = [SingleEvent]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         loadEvents()
         initSwipes()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
@@ -54,14 +54,14 @@ class EventTableViewController: UITableViewController {
         //queryExpression.filterExpression = "(contains(Event_Name, :event_name))"
         //queryExpression.expressionAttributeValues = [":event_name": "Train"]
         
-       
+        
         
         self.scan(queryExpression).continueWithBlock({ (task: AWSTask!) -> AWSTask! in
             if task.result != nil {
                 let paginatedOutput = task.result as! AWSDynamoDBPaginatedOutput
                 for item in paginatedOutput.items as! [SingleEvent] {
                     self.events.append(item)
-                   //print(item)
+                    //print(item)
                     //self.downloadImage(NSURL(string: (item.Event_Picture_Link)!)!)
                     
                     
@@ -71,7 +71,7 @@ class EventTableViewController: UITableViewController {
             
             dispatch_async(dispatch_get_main_queue(), {
                 self.tableView.reloadData()
-            
+                
             })
             
             
@@ -88,44 +88,44 @@ class EventTableViewController: UITableViewController {
         return mapper.scan(SingleEvent.self, expression: expression)
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: Segue functions 
+    // MARK: Segue functions
     override func prepareForSegue(segue:UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "EventViewSegue" {
             if let nav = segue.destinationViewController as? UINavigationController{
                 if let dvc = nav.topViewController as? EventPageViewController{
-                dvc.currentEvent = self.sendEvent
+                    dvc.currentEvent = self.sendEvent
                     
                 }
             }
         } else if segue.identifier == "LeftSwipe" {
             if let nav = segue.destinationViewController as? UINavigationController{
                 if let dvc = nav.topViewController as? FilterViewController{
-                        // send some data
+                    // send some data
                 }
             }
         } else if segue.identifier == "RightSwipe" {
             if let nav = segue.destinationViewController as? FavoritesTableViewController {
-                    // do something with nav data
-                for fav in favorites {
-                        nav.favorites.append(fav)
+                // do something with nav data
+                for fave in self.favorites{
+                    nav.favorites.append(fave)
                 }
             }
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return events.count
@@ -138,7 +138,7 @@ class EventTableViewController: UITableViewController {
         //dvc.currentEvent = sendEvent
         self.performSegueWithIdentifier("EventViewSegue", sender: self)
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier = "EventTableViewCell"
@@ -154,20 +154,20 @@ class EventTableViewController: UITableViewController {
         
         NSURLSession.sharedSession().dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void in
             dispatch_async(dispatch_get_main_queue()) { () -> Void in
-
+                
                 guard let data = data where error == nil else { return }
                 print(response?.suggestedFilename ?? "")
                 print("download Finished")
-            
+                
                 cell.eventImage.image = UIImage(data: data)
             }
-
-        }.resume()
+            
+            }.resume()
         
         //cell.eventImage.image = self.eventTempImage
-
+        
         // Configure the cell...
-
+        
         return cell
     }
     
@@ -183,22 +183,22 @@ class EventTableViewController: UITableViewController {
                 guard let data = data where error == nil else { return }
                 print(response?.suggestedFilename ?? "")
                 print("download Finished")
-        
+                
                 self.eventTempImage = UIImage(data: data)
                 
             }
             
             /*dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
-                
-            }*/
+             self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
+             
+             }*/
             
         }
         
         
     }
     
-
+    
     
     func handleSwipe(sender: UIScreenEdgePanGestureRecognizer) {
         if (sender.state == .Recognized) {
@@ -206,17 +206,58 @@ class EventTableViewController: UITableViewController {
                 performSegueWithIdentifier("RightSwipe", sender: self)
             } else if (sender.edges == .Left) {
                 performSegueWithIdentifier("LeftSwipe", sender: self)
- 
+                
             }
         }
     }
     
     @IBAction func unwindFromFilters(sender: UIStoryboardSegue) {
-        //insert some data here 
+        //insert some data here
         if sender.identifier == "LeftSwipe" {
             
         }
     }
+    
+    
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    
+
     
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         var favoriteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "U are my favorite", handler: {action, indexpath in
@@ -242,35 +283,6 @@ class EventTableViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }*/
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
 
 }
