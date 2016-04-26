2067b5549fa04d2a11c835480923085586ab95d0
brad-bergeron <admin@74-80-249-108.ic2net.net>
2016-04-26 13:28:19 -0700

    Fixed the sorting of events again, finally thinks it works

--- Iconic/EventTableViewController.swift
+++ Iconic/EventTableViewController.swift
@@ -80,30 +80,32 @@ class EventTableViewController: UITableViewController {
                     let paginatedOutput = task.result as! AWSDynamoDBPaginatedOutput
                     var count = 0
                     for item in paginatedOutput.items as! [SingleEvent] {
-                        self.filteredEvents.append(item)
                         self.events.append(item)
-                        print(self.events[count].Event_Picture_Link!)
+                        
                         if((count < self.events.count) && (self.events[count].Event_Picture_Link! != " ")){
                             self.downloadImage(NSURL( string: self.events[count].Event_Picture_Link!)!, count: count) { (result) -> () in
                                 if(result==true){
+                                    self.filteredEvents = self.events
+                                    self.filteredEvents.sortInPlace({ $0.Event_NSDate!.compare($1.Event_NSDate!) == NSComparisonResult.OrderedAscending })
                                     self.tableView.reloadData()
                                 }
                             }
                             
                         }
-                       let dat = self.stringToDate(self.events[count].Event_Date_Formatted!, time: self.events[count].Event_Time!)
-                         self.events[count].Event_NSDate = dat
+                        let dat = self.stringToDate(self.events[count].Event_Date_Formatted!, time: self.events[count].Event_Time!)
+                        self.events[count].Event_NSDate = dat
+                       
                         
                         count = count + 1
                     }
             }
             
-            dispatch_async(dispatch_get_main_queue(), {
-                self.filteredEvents.sortInPlace({ $0.Event_NSDate!.compare($1.Event_NSDate!) == NSComparisonResult.OrderedAscending })
+            /*dispatch_async(dispatch_get_main_queue(), {
+                /*self.filteredEvents.sortInPlace({ $0.Event_NSDate!.compare($1.Event_NSDate!) == NSComparisonResult.OrderedAscending })*/
                 self.tableView.reloadData()
                 self.loaded = true
                 
-            })
+            })*/
             
             
             if ((task.error) != nil) {
@@ -246,9 +248,6 @@ class EventTableViewController: UITableViewController {
                 print("download Finished")
                 
                 self.events[count].Event_Picture = UIImage(data: data)
-                if(count < self.filteredEvents.count){
-                    self.filteredEvents[count].Event_Picture = UIImage(data: data)
-                }
                 
                 completion(result: true)
             }
