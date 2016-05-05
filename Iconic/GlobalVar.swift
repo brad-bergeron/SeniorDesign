754dc8cdf018a501fc4b07a31d69db7e5afa6179
cmdanielson <caitlin-danielson@uiowa.edu>
2016-05-05 12:27:03 -0500

    merging

--- Iconic/GlobalVar.swift
+++ Iconic/GlobalVar.swift
@@ -37,3 +37,10 @@ let blueMooseLat = 41.660991
 let blueMooseLong = -91.532574
 let cabLat = 41.663034
 let cabLong = -91.538435
+let poorString = "to be implemented"
+
+let helpHome = "Swipe left for filters \n Swipe right for favorites \n Swipe right in a cell to favorite or unfavorite an event, favorited events are outlined in Iconic orange\n Click on a cell to view more details about the event"
+
+let helpFav = "Once an event is favorited, it will appear here \n Click on the image to view more details about the event \n Favorite or unfavorite an event by dragging within a cell on the home page or by clicking the star in the in the event details page \n Favorited events appear Iconic orange"
+
+let helpFilt = "Enable filters by clicking the switch \n Activate a filter by tapping on them, filter icon will appear white and filter name will appear on the bottom bar \n Turn a filter off by tapping the active filter, filter icon will appear gray \n "
